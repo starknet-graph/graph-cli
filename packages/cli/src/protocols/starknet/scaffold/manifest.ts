3735cf254a1e39f282ce8d760294072ce88809d5
zkmg <108724835+zkmg@users.noreply.github.com>
2023-02-13 10:48:23 +0800

    feat: add support for starknet codegen from abi (pr 3)
    Co-authored-by: Jonathan LEI <me@xjonathan.dev>

--- packages/cli/src/protocols/starknet/scaffold/manifest.ts
+++ packages/cli/src/protocols/starknet/scaffold/manifest.ts
@@ -1,11 +1,27 @@
-export const source = ({ startBlock }: { startBlock?: string }) =>
-  `startBlock: ${startBlock ?? '0'}`;
+import { strings } from 'gluegun';
+import { abiEvents } from '../../../scaffold/schema';
+import ABI from '../abi';
 
-export const mapping = () => `
-      apiVersion: 0.0.5
+export const source = ({ contract, startBlock }: { contract: string; startBlock?: string }) => `
+      address: '${contract}'
+      startBlock: ${startBlock ?? '0'}`;
+
+export const mapping = ({ abi, contractName }: { abi: ABI; contractName: string }) => `
+      apiVersion: 0.0.6
       language: wasm/assemblyscript
       entities:
-        - ExampleEntity
-      blockHandlers:
-        - handler: handleBlock
-      file: ./src/contract.ts`;
+        ${abiEvents(abi)
+          .map(event => `- ${event.get('_alias')}`)
+          .join('\n        ')}
+      abis:
+        - name: ${contractName}
+          file: ./abis/${contractName}.json
+      eventHandlers:
+        ${abiEvents(abi)
+          .map(
+            event => `
+        - event: ${ABI.eventSignature(event)}
+          handler: handle${event.get('_alias')}`,
+          )
+          .join('')}
+      file: ./src/${strings.kebabCase(contractName)}.ts`;
