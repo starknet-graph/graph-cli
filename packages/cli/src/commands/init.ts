75cf6336ad2067573cb80b292f628fdd774189fd
Jonathan LEI <me@xjonathan.dev>
2023-09-08 04:24:09 +0000

    feat: do not index event entities by default

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -710,7 +710,7 @@ async function processInitForm(
         type: 'confirm',
         name: 'indexEvents',
         message: 'Index contract events as entities',
-        initial: true,
+        initial: false,
         skip: () => !!initIndexEvents || isSubstreams,
       },
     ]);
